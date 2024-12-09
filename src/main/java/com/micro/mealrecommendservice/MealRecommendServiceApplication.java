2388458da1272df8919034e7d2723c0fa503501c
iammksmohit <mohitchemistry123@gmail.com>
2024-12-09 20:03:28 +0530

    init repo  created

---
+++ src/main/java/com/micro/mealrecommendservice/MealRecommendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.mealrecommendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealRecommendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealRecommendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/micro/mealrecommendservice/MealRecommendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.mealrecommendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealRecommendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
